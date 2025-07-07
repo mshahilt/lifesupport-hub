699af9fdd7fad2f2918539100f336ac59f40211e
mshahilt <mshahilt3@gmail.com>
2025-07-07 20:46:35 +0530

    icon fix_1

--- client/src/app/dashboard/components/ActiveLessonsList.tsx
+++ client/src/app/dashboard/components/ActiveLessonsList.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Card } from '@/components/ui/Card';
 import { LiveSession } from '@/types';
-import Image from 'next/image';
 
 
 interface ActiveLessonsListProps {
